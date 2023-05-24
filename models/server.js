846883984e9a28d1f8ea8e0befad839e16f44f1d
ezemg <ezequielmatiasgr@gmail.com>
2023-05-24 17:15:25 -0300

    Trabajo sobre auth de usuario y generacion de JWT

--- models/server.js
+++ models/server.js
@@ -7,6 +7,7 @@ class Server {
     this.app = express();
     this.port = process.env.PORT;
     this.usuariosPath = '/api/usuarios';
+    this.authPath = '/api/auth';
 
     // Conectar a DB
     this.conectarDB();
@@ -34,6 +35,8 @@ class Server {
   }
 
   routes() {
+    this.app.use(this.authPath, require('../routes/authPathRoutes.js'));
+
     this.app.use(this.usuariosPath, require('../routes/usuariosRoutes.js'));
   }
 
